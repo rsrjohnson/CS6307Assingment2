8e053f2c142a4d8f9b2d49b35a74a7c195324eb8
Randy Suarez Rodes <rsrjohnson@gmail.com>
2021-04-02 01:07:56 -0500

    Add instructions, add comments, clean extra code

--- CS6307_Assingment2.scala
+++ CS6307_Assingment2.scala
@@ -1,4 +1,7 @@
 // Databricks notebook source
+//CS6307_Assingment 2
+//Team Members:Randy Suarez Rodes rxs179030 and Ping Chen pxc190026
+
 //Part 1
 
 //Packages and libraries
@@ -31,11 +34,11 @@ val embeddings = WordEmbeddingsModel.pretrained()
    .setOutputCol("embeddings")
 
 val ner = NerDLModel.pretrained()
-  .setInputCols("document", "token", "embeddings")
+  .setInputCols("document", "normal", "embeddings")
   .setOutputCol("ner")
 
 val nerConverter = new NerConverter()
-  .setInputCols("document", "token", "ner")
+  .setInputCols("document", "normal", "ner")
   .setOutputCol("entities")
 
 val finisher = new Finisher()
@@ -67,16 +70,15 @@ val book_orig = sc.textFile("/FileStore/tables/sherlock_holmes.txt").filter(row 
 
 val bookDF=book_orig.toDS.toDF( "text") //preparing data frame to be transformed
 
-val result = pipeline.fit(bookDF).transform(bookDF) //pipeline is already pre-trained, we only need to fit
+val result = pipeline.fit(bookDF).transform(bookDF) //pipeline is already pre-trained, we only need to fit pur data frame
 
 val named_entities=result.select($"finished_entities").as[String].rdd // getting named entities and converting to rdd
 
+
 // COMMAND ----------
 
-val words=named_entities.flatMap(line => line.split(",")).map(_.replaceAll("[\\W&&[^']]", " ")).map(_.trim).filter(x=> x.length>0) //joining each line, removing selected non alphanumeric characters, trimming and removing empty characters
-
-//val final_words=words.map(_.toLowerCase).map(_.replaceAll("'",""))
-//val words=named_entities.flatMap(line => line.split(",")).map(_.replaceAll("\\W", " ")).map(_.trim).filter(x=> x.length>0) 
+//joining each line, removing special characters , replacing non alphanumeric characters with space, trimming and removing empty and single characters
+val words=named_entities.flatMap(line => line.split(",")).map(_.replaceAll("'`¨“”", "")).map(_.replaceAll("\\W+", " ")).map(_.replaceAll("_","")).map(_.trim).filter(x=> x.length>1)
 
 val wordsCount=words.map(x => (x,1)).reduceByKey((x,y) => x+y) //finding words counts
 
@@ -86,18 +88,15 @@ wordsCount.sortBy(-_._2).collect() //reduce operation, displaying word count in 
 
 // COMMAND ----------
 
-|//Part 2
+//Part 2
 
-//import org.apache.spark.ml.feature.{RegexTokenizer, Tokenizer}
 import org.apache.spark.sql.functions._
 import org.apache.spark.ml.feature.StopWordsRemover
 import spark.implicits._
 import scala.math._
 
-
-
-//separating id and plot, removing non alpha numeric characters except apostrophe, converting to lower case, wrapping all into a DF 
-var movie_plots=sc.textFile("/FileStore/tables/plot_summaries.txt").map(line => line.split("\t")).map(x=>(x(0),x(1).replaceAll("[\\W&&[^']]"," ").split(" ").filter(word=>word.length>0).map(_.toLowerCase))).toDF("id","plot")
+//separating id and plot, changing non alpha numeric characters (except apostrophe) to " ", replacing apostrophe by "",  converting to lower case, wrapping all into a DF 
+var movie_plots=sc.textFile("/FileStore/tables/plot_summaries.txt").map(line => line.split("\t")).map(x=>(x(0),x(1).replaceAll("[\\W&&[^']]"," ").replaceAll("'","").split(" ").filter(word=>word.length>1).map(_.toLowerCase))).toDF("id","plot")
 
 
 //Stop Words Remover
@@ -109,64 +108,46 @@ movie_plots=remover.transform(movie_plots).drop("plot") //removing stopwords and
 
 val N=movie_plots.count //42306 films
 
-display(movie_plots)
-
-
 
 // COMMAND ----------
 
+//Function to create a dictionary of word counts for each document
 def arr_to_dict(words:Seq[String]):scala.collection.immutable.Map[String,Int]={
   
   return words.groupBy(identity).mapValues(_.length)
    
 }
+
 val arr_to_dictUDF = udf(arr_to_dict _)
 
 //Dictionary of each word count per document to be used for cosine similarity 
 val words_dict=movie_plots.withColumn("words_dict",arr_to_dictUDF(col("plot_filtered"))).drop("plot_filtered")
 
-display(words_dict)
-
 // COMMAND ----------
 
 val flattened = movie_plots.withColumn("token",explode($"plot_filtered")).drop("plot_filtered")
 
-//val lltokens = flattened.groupBy("token")
-
-//finding term i frequencies by document j
+//finding term i frequencies in document j
 val TF = flattened.groupBy("id", "token").count().toDF("id","token","tf_ij")
-
-TF.show
-//display(TF.filter($"token".contains("haymitch")))
 
 //finding number of documents of every term
 val DF = flattened.distinct().groupBy("token").count().toDF("token","df")
 
-DF.show
-
-
-//display(DF.filter($"token".contains("haym")))
-
 // COMMAND ----------
 
-val idfFunc = udf((df:Long) =>log(42306/df))
+//Function to find a term idf
+val idfFunc = udf((df:Long) =>log(N/df))
 
 val newdf = DF.withColumn("idf",idfFunc(col("df")))
 
-newdf.show(false)
-
-//join TF and newdf, calculate the tf-idf
+//Join TF and newdf to calculate the tf-idf
 val tf_idfDF = TF
       .join(newdf, Seq("token"))
       .withColumn("tf_idf", col("tf_ij") * col("idf")).select("id","token","tf_idf")
 
-
-tf_idfDF.show(false)
-
-
 // COMMAND ----------
 
-//Euclidean Norm 
+//Fucntion to calculate Euclidean Norm for arrays
 def norm(v:Seq[Double]):Double={
     
   sqrt((v).map { case (x) => pow(x, 2) }.sum)
@@ -174,9 +155,7 @@ def norm(v:Seq[Double]):Double={
 
 val normUDF = udf(norm _)
 
-
-
-//Euclidean Norm 
+//Fucntion to calculate Euclidean Norm  for dictionary of counts
 def normD(v:Map[String,Int] ):Double={
     
   sqrt((v.values).map { case (x) => pow(x, 2) }.sum)
@@ -188,51 +167,28 @@ val normDUDF = udf(normD _)
 
 // COMMAND ----------
 
-//val tf_idfvectors=tf_idfDF.groupBy("id").agg(collect_list("tf_idf").as("tf_idf_vector"))
-
+//Finding the norms of each document
 val tf_vectors=TF.groupBy("id").agg(collect_list(col("tf_ij").cast("double")).as("tf_vector"))
-//finding the norm of each document
-//tf_idfvectors.withColumn("norm",normUDF(col("tf_idf_vector"))).show
-
 val document_norms = tf_vectors.withColumn("norm",normUDF(col("tf_vector"))).drop("tf_vector")
 
-//document_norms.show(false)
-
-
+//Dataframe of norms
 val normDF=words_dict.join(document_norms,Seq("id"))
-
-normDF.show
 
 // COMMAND ----------
 
-//val terms = sc.textFile("/FileStore/tables/user_terms.txt").map(_.toLowerCase).map(x=>x.split(" "))
-
+//User's terms
 var terms = sc.textFile("/FileStore/tables/user_terms.txt").map(_.toLowerCase).map(x=>x.split(" ")).toDF("query")
 
+//removing stop words from queries
 val remover2 = new StopWordsRemover()
   .setInputCol("query")
   .setOutputCol("query_filtered")
 
-//removing stop words from queries
 terms=remover2.transform(terms).drop("query") //removing stopwords and dropping not processed queries
 
-
-
-
-terms.show(false)
-
-
 // COMMAND ----------
 
-terms=terms.withColumn("words_dict",arr_to_dictUDF(col("query_filtered")))
-
-
-
-terms=terms.withColumn("norm",normDUDF(col("words_dict")))
-
-
-// COMMAND ----------
-
+//Function to find the dot product of two vectors
 def dot_prod(term_dic:Map[String,Int],doc_dict:Map[String,Int]):Double=
 {
   var result=0
@@ -248,30 +204,31 @@ def dot_prod(term_dic:Map[String,Int],doc_dict:Map[String,Int]):Double=
 
 val dot_prodUDF = udf(dot_prod _)
 
-
-
 // COMMAND ----------
 
+//Function to find the top 10 movies based on the user's terms
 def query_results(search:Array[String],  tf_idfDF: org.apache.spark.sql.DataFrame,normDF:org.apache.spark.sql.DataFrame):org.apache.spark.sql.DataFrame={
    
+  //Creating a dictionary of word counts from the query
   val query_dict=search.groupBy(identity).mapValues(_.length)
   
+  //Single term queries
   if(query_dict.size==1){
     
-    //single term query
-    val qDF = tf_idfDF.filter($"token"===query_dict.keys.toList(0) )   
+      val qDF = tf_idfDF.filter($"token"===query_dict.keys.toList(0) )   
     
+    //returning tf_idf
     return qDF.orderBy(desc("tf_idf")).limit(10)    
   }
-  else{    
-      
-  
-    val ext_Norm=normDF.withColumn("query_dict",typedLit(query_dict))
+  else{ 
     
-    
-    
+     //Norm of the query for cosine similarity
     val query_norm=sqrt((query_dict.values).map { case (x) => pow(x, 2) }.sum)
     
+    //Adding the query dictionary to a column of the documents data frame    
+    val ext_Norm=normDF.withColumn("query_dict",typedLit(query_dict))      
+    
+    //Finding the cosine similarity between the query and every document    
     val qDF=ext_Norm.withColumn("cosine_sim",dot_prodUDF('query_dict,'words_dict)/('norm*query_norm)).select("id","cosine_sim")
     
     return qDF.orderBy(desc("cosine_sim")).limit(10)  
@@ -281,40 +238,23 @@ def query_results(search:Array[String],  tf_idfDF: org.apache.spark.sql.DataFram
 
 // COMMAND ----------
 
-//answer to the user terms, we use this answers to look for the movie name
-val answers=terms.select($"query_filtered").as[Array[String]].rdd.collect.map(x=>query_results(x,tf_idfDF,normDF))
+//Answers to the user's terms, later we use this answers to look for the movie name
+val answers=terms_processed.select($"query_filtered").as[Array[String]].rdd.collect.map(x=>query_results(x,tf_idfDF,normDF))
 
 // COMMAND ----------
 
-val flat_terms = terms.withColumn("token",explode($"query_filtered"))
-
-//preparing the terms for cosine similarity
-val queries_weights = flat_terms.groupBy("query_filtered", "token").count().toDF("query","token","weight")
-
-queries_weights.show(30,false)
-
-//finding norms of the queries
-val queries_norms=queries_weights.groupBy("query").agg(normUDF(collect_list(col("weight").cast("double"))).as("norm"))
-
-queries_norms.show(false)
-
-// COMMAND ----------
-
-//val movie_meta=sc.textFile("/FileStore/tables/movie_metadata.tsv")
-
+//Columns' names for the movie_metadata dataframe
 val newColumns = Seq("Wikipedia movie ID","Freebase movie ID", "Movie name","Movie release date","Movie box office revenue","Movie runtime","Movie languages","Movie countries","Movie genres")
 
-
+//Movie_metadata dataframe
 val movie_meta=spark.read.option("sep", "\t").option("inferSchema","true").csv("/FileStore/tables/movie_metadata.tsv").toDF(newColumns:_*)
-
-
-
-display(movie_meta)
 
 // COMMAND ----------
 
+//Final answers to all the queries
+
 var k=0
-//answer to all the queries
+//We join the top documents id of each query with the movie id
 for(query<-terms.select("query_filtered").as[Array[String]].collect){
   println("Query: " + query.mkString(", "))
   if(query.length==1)
